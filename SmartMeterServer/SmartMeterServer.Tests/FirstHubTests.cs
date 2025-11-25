77fe1ed89a4d7e4170fd090225e70b293149553e
c3070189 <c3070189@hallam.shu.ac.uk>
2025-11-25 00:22:48 +0000

    addedd some tests NOT ALL

---
+++ SmartMeterServer/SmartMeterServer.Tests/FirstHubTests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+using SmartMeterServer.Hubs;
+using SmartMeterServer.Models;
+using SmartMeterServer.Logging;
+using Microsoft.AspNetCore.SignalR;
+using Moq;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SmartMeterServer.Tests
+{
+    internal class FirstHubTests
+    {
+        public async Task OnDisconnectedAsync_ShouldLogClientDisconnected()
+        {
+            // Arrange
+            File.Delete("server_logs.csv");
+
+            var storeMock = new Mock<IMeterStore>();
+            var hub = new FirstHub(storeMock.Object);
+
+            var contextMock = new Mock<HubCallerContext>();
+            contextMock.Setup(c => c.ConnectionId).Returns("TEST123");
+
+            // Inject mock Context
+            typeof(Hub)
+                .GetProperty("Context")
+                .SetValue(hub, contextMock.Object);
+
+            // Act
+            await hub.OnDisconnectedAsync(null);
+
+            // Assert
+            var lines = File.ReadAllLines("server_logs.csv");
+            Assert.Contains("CLIENT_DISCONNECTED", lines[1]);
+            Assert.Contains("TEST123", lines[1]);
+        }
+}
---
+++ SmartMeterServer/SmartMeterServerLog/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SmartMeterServerLog
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
